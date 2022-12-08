1d0f2bf7d51432faee06d7e1c70fb02ffc8e2fc1
jonathan4idps <jonathan@4idps.com>
2022-12-08 11:36:10 +0800

    feat: add workflow

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     //handle icomming request here...
     if (req.url == '/') {
+
+        console.log('root url been called...');
+
         res.writeHead(200, {'Content-Type': 'application/json'});
 
         res.write('<html><body><p>This is home page.</p></body></html>');
